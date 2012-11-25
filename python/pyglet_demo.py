42581db960e967aa1a32569042fdea17a12e0a81
Brandon <brandon.light@gmail.com>
2012-11-25 13:53:59 -0500

    Python: Remove unneccessary GL calls in pyglet_demo

--- python/pyglet_demo.py
+++ python/pyglet_demo.py
@@ -1,15 +1,11 @@
 
-
 import pyglet
-import pyglet.gl
 
 window = pyglet.window.Window()
 image = pyglet.resource.image('kitten.png')
 
 @window.event
 def on_draw():
-        pyglet.gl.glClearColor(1.0, 1.0, 0.0, 1.0)
-        pyglet.gl.glClear(pyglet.gl.GL_COLOR_BUFFER_BIT)
         image.blit(0,0)
 
 pyglet.app.run()
